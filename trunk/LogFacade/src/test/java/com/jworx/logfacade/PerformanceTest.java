afde954342e7dfc07d90416b53c57c14723775ee
dalbrekt <dalbrekt@d496d23e-c23f-0410-82a1-bfcf1d229d22>
2007-11-25 18:07:36 +0000

    Increased number of times to run the performance test loop

--- trunk/LogFacade/src/test/java/com/jworx/logfacade/PerformanceTest.java
+++ trunk/LogFacade/src/test/java/com/jworx/logfacade/PerformanceTest.java
@@ -9,7 +9,7 @@ public class PerformanceTest {
     public void tryDebugLog() {
 
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             log.trace("Formatted %s: count %d", "log", i);
         }
         System.out.println("Time1: " + (System.currentTimeMillis() - start));
