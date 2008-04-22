0786a6499ae8dc1908c16fd7bd846963389a010d
dalbrekt <dalbrekt@d496d23e-c23f-0410-82a1-bfcf1d229d22>
2008-04-22 20:00:25 +0000

    Create release branch for RB-1.1

---
+++ branches/RB-1.1/LogFacade/src/test/java/com/jworx/logfacade/PerformanceTest.java
@@ -0,0 +1,18 @@
+package com.jworx.logfacade;
+
+import org.junit.Test;
+
+public class PerformanceTest {
+	static Log log = LogFactory.getLog(PerformanceTest.class);
+
+	@Test
+	public void tryDebugLog() {
+
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 1000000; i++) {
+			log.trace("Formatted %s: count %d", "log", i);
+		}
+		System.out.println("Time1: " + (System.currentTimeMillis() - start));
+
+	}
+}
